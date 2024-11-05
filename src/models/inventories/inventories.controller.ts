2057acd83e859ce210d66e4f62722f7f6c4e0260
Nguyen Gia Hau <gia.hau.nguyen.2004@gmail.com>
2024-11-05 11:57:13 +0700

    update decorator for inventories controller from MESSAGE QUEUE to REST API

--- src/models/inventories/inventories.controller.ts
+++ src/models/inventories/inventories.controller.ts
@@ -1,5 +1,4 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { InventoriesService } from './inventories.service';
 import { CreateInventoryDto } from './dto/create-inventory.dto';
 import { UpdateInventoryDto } from './dto/update-inventory.dto';
@@ -9,28 +8,28 @@ export class InventoriesController {
   constructor(private readonly inventoriesService: InventoriesService) { }
 
 
-  @MessagePattern({ cmd: 'inventories', action: 'create' })
-  create(@Payload() createInventoryDto: CreateInventoryDto) {
+  @Post()
+  create(@Body() createInventoryDto: CreateInventoryDto) {
     return this.inventoriesService.create(createInventoryDto);
   }
 
-  @MessagePattern({ cmd: 'inventories', action: 'findAll' })
+  @Get()
   findAll() {
     return this.inventoriesService.findAll();
   }
 
-  @MessagePattern({ cmd: 'inventories', action: 'findOne' })
-  findOne(@Payload() id: number) {
+  @Get('/:id')
+  findOne(@Param() id: number) {
     return this.inventoriesService.findOne(id);
   }
 
-  @MessagePattern({ cmd: 'inventories', action: 'update' })
-  update(@Payload() updateInventoryDto: UpdateInventoryDto) {
-    return this.inventoriesService.update(updateInventoryDto.id, updateInventoryDto);
+  @Put('/:id')
+  update(@Param() id: number, @Body() updateInventoryDto: UpdateInventoryDto) {
+    return this.inventoriesService.update(id, updateInventoryDto);
   }
 
-  @MessagePattern({ cmd: 'inventories', action: 'remove' })
-  remove(@Payload() id: number) {
+  @Delete('/:id')
+  remove(@Param() id: number) {
     return this.inventoriesService.remove(id);
   }
 }
