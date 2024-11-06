b8d24d692cfa776d59bad4bb4813e7540f0cfa03
Nguyen Gia Hau <gia.hau.nguyen.2004@gmail.com>
2024-11-06 10:46:54 +0700

    fix: application running on wrong port

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppConfigService } from './config/app/config.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
+  // config swagger 
+  // const config = new DocumentBuilder()
+  //   .setTitle('Cats example')
+  //   .setDescription('The cats API description')
+  //   .setVersion('1.0')
+  //   .addTag('cats')
+  //   .build();
+  // const documentFactory = () => SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('api', app, documentFactory);
+
   // get app config for cors serttings and starting the app.
   const appConfig: AppConfigService = app.get(AppConfigService)
-
-  await app.listen(appConfig.port, () => console.log(`Asset service is running on: `, {
+  await app.listen(appConfig.servicePort, () => console.log(`Asset service is running on: `, {
     host: appConfig.serviceHost,
     port: appConfig.servicePort
   }));
