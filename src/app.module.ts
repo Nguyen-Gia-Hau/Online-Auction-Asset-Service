555e2d40dc420519bca03ee9b0613d66973d075d
Nguyen Gia Hau <gia.hau.nguyen.2004@gmail.com>
2024-11-06 10:43:17 +0700

    remove nofication when user send request

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,7 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/config.module';
-import { LoggingMiddleware } from './middleware/logging.middleware';
 import { MariadbDatabaseProviderModule } from './providers/database/mariadb/provider.module';
 import { AssetTypesModule } from './models/asset-types/asset-types.module';
 import { AssetStatusesModule } from './models/asset-statuses/asset-statuses.module';
@@ -24,8 +23,4 @@ import { AssetsModule } from './models/assets/assets.module';
   providers: [AppService],
 })
 
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggingMiddleware).forRoutes('*');
-  }
-}
+export class AppModule { }
