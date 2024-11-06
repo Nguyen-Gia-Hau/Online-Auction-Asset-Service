5f553233e85b08dee9a354c532bed8931b2b1054
Nguyen Gia Hau <gia.hau.nguyen.2004@gmail.com>
2024-11-06 19:46:30 +0700

    update: add description swagger for asset statsus

--- src/models/asset-statuses/asset-statuses.controller.ts
+++ src/models/asset-statuses/asset-statuses.controller.ts
@@ -1,34 +1,158 @@
+
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { AssetStatusesService } from './asset-statuses.service';
 import { CreateAssetStatusDto } from './dto/create-asset-status.dto';
 import { UpdateAssetStatusDto } from './dto/update-asset-status.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AssetStatus } from './entities/asset-status.entity';
 
+@ApiTags('Asset Statuses')
 @Controller('asset-statuses')
 export class AssetStatusesController {
   constructor(private readonly assetStatusesService: AssetStatusesService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Create asset status' })
+  @ApiResponse({
+    status: 201,
+    description: 'Asset status created successfully',
+    schema: {
+      example: {
+        code: 201,
+        message: 'Asset status created successfully',
+        metadata: {
+          assetStatusID: 1,
+          assetStatusName: 'asset status name 1',
+          delflag: false,
+          created_at: '1730889366606',
+          updated_at: '1730889366607',
+          deleted_at: null,
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        code: 500,
+        message: 'Internal server error occurred',
+      },
+    },
+  })
   create(@Body() createAssetStatusDto: CreateAssetStatusDto) {
     return this.assetStatusesService.create(createAssetStatusDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Retrieve all asset statuses' })
+  @ApiResponse({
+    status: 200,
+    description: 'Asset statuses retrieved successfully',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        code: 500,
+        message: 'Internal server error occurred',
+      },
+    },
+  })
   findAll() {
     return this.assetStatusesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retrieve an asset status by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Asset status retrieved successfully',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Asset status not found',
+    schema: {
+      example: {
+        code: 404,
+        message: 'Asset status with ID 1 not found',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        code: 500,
+        message: 'Internal server error occurred',
+      },
+    },
+  })
   findOne(@Param('id') id: number) {
     return this.assetStatusesService.findOne(id);
   }
 
   @Put(':id')
+  @ApiOperation({ summary: 'Update an asset status by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Asset status updated successfully',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Asset status not found',
+    schema: {
+      example: {
+        code: 404,
+        message: 'Asset status with ID 1 not found',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        code: 500,
+        message: 'Internal server error occurred',
+      },
+    },
+  })
   update(@Param('id') id: number, @Body() updateAssetStatusDto: UpdateAssetStatusDto) {
     return this.assetStatusesService.update(id, updateAssetStatusDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete an asset status by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Asset status deleted successfully',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Asset status not found',
+    schema: {
+      example: {
+        code: 404,
+        message: 'Asset status with ID 1 not found',
+      },
+    },
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+    schema: {
+      example: {
+        code: 500,
+        message: 'Internal server error occurred',
+      },
+    },
+  })
   remove(@Param('id') id: number) {
     return this.assetStatusesService.remove(id);
   }
 }
+
--- src/models/asset-statuses/entities/asset-status.entity.ts
+++ src/models/asset-statuses/entities/asset-status.entity.ts
@@ -1,22 +1,42 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class AssetStatus {
   @PrimaryGeneratedColumn()
+  @ApiProperty({
+    example: 1,
+    description: 'ID of asset status'
+  })
   assetStatusID: number;
 
   @Column()
+  @ApiProperty({
+    example: 'asset status name 1',
+    name: 'The name of the asset status'
+  })
   assetStatusName: string;
 
   @Column({ default: false, nullable: false })
+  @ApiProperty({
+    example: false,
+    description: 'Status delete of asset status'
+  })
   delflag: boolean;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  @ApiProperty({
+    example: Date.now()
+  })
   created_at: Date;
 
   @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP' })
+  @ApiProperty({
+    example: Date.now()
+  })
   updated_at: Date;
 
   @Column('timestamp', { nullable: true })
+  @ApiProperty({})
   deleted_at: Date;
 }
